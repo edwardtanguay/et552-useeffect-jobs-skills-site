6cc51c742cabfc44c7d025bd3ab282948ced21c7
Edward Tanguay (windows) <edward@tanguay.info>
2023-11-08 00:21:32 +0100

    jobs in

--- src/App.tsx
+++ src/App.tsx
@@ -15,8 +15,15 @@ function App() {
 
 	return (
 		<>
-			<h1 className="text-3xl mb-3 text-slate-800">Info Site</h1>
-			<p>There are {jobs.length} jobs.</p>
+			<h1 className="text-3xl mb-3 text-slate-800">Jobs/Skills Site</h1>
+			<h2 className="text-xl mb-3">There are {jobs.length} jobs.</h2>
+			<div>
+				{jobs.map(job => {
+					return (
+						<p className="bg-slate-300 p-2 mb-2 w-[20rem] rounded">{job.title}</p>
+					)
+				})}
+			</div>
 		</>
 	);
 }
